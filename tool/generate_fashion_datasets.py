4e209f9c0aa3c9edaf8eda86a3e3a60e31213f7f
Sebahammer97 <55815143+Sebahammer97@users.noreply.github.com>
2021-07-17 02:12:52 -0300

    Data set split and crop

--- tool/generate_fashion_datasets.py
+++ tool/generate_fashion_datasets.py
@@ -1,7 +1,15 @@
 import os
+from shutil import copyfile
+from pathlib import Path
+from PIL import Image
+
+def crop_image(img_dir, save_dir):
+	img = Image.open(img_dir)
+	imgcrop = img.crop((40, 0, 216, 256))
+	imgcrop.save(save_dir)
 
 # path for downloaded fashion images
-root_fashion_dir = 'your_path/deepfashion'
+root_fashion_dir = 'D:\\Archivos UADE\\PFI\\Desarrollos\\ADGAN\\deepfashion'
 assert len(root_fashion_dir) > 0, 'please give the path of raw deep fashion dataset!'
 
 train_images = []
@@ -23,16 +31,33 @@ if not os.path.exists(train_path):
 	os.mkdir(train_path)
 
 for item in train_images:
-	from_ = os.path.join(root_fashion_dir, item)
-	to_ = os.path.join(train_path, item)
-	os.system('cp %s %s' %(from_, to_))
-
+	id_index = item.index("id_0")
+	path = item.replace(item[id_index + 11 :], "\\" + item[id_index + 11 :])
+	from_ = os.path.join(root_fashion_dir, path)
+	from_ = from_.replace("\\", "/")
+	to_ = os.path.join(train_path, path)
+	to_ = to_.replace("\\", "/")
+#	to_array = to_.split("/")
+#	to_array.pop()
+#	Path("/".join(to_array)).mkdir(parents=True, exist_ok=True)
+	#os.system('copy %s %s' %(from_, to_))
+#	copyfile(from_, to_)
+	crop_image(from_, to_)
 
 test_path = os.path.join(root_fashion_dir,'test')
 if not os.path.exists(test_path):
 	os.mkdir(test_path)
 
 for item in test_images:
-	from_ = os.path.join(root_fashion_dir, item)
-	to_ = os.path.join(test_path, item)
-	os.system('cp %s %s' %(from_, to_))
+	id_index = item.index("id_0")
+	path = item.replace(item[id_index + 11 :], "\\" + item[id_index + 11 :])
+	from_ = os.path.join(root_fashion_dir, path)
+	from_ = from_.replace("\\", "/")
+	to_ = os.path.join(test_path, path)
+	to_ = to_.replace("\\", "/")	
+	#to_array = to_.split("/")
+	#to_array.pop()
+	#Path("/".join(to_array)).mkdir(parents=True, exist_ok=True)
+	#os.system('copy %s %s' %(from_, to_))
+	#copyfile(from_, to_)
+	crop_image(from_, to_)
